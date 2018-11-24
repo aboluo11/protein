894d2f7441236b2103e622b732bf1db7a140d7e7
Apollo <sushi90817564@gmail.com>
2018-11-24 20:26:27 +0800

    add pretrained option

--- src/model.py
+++ src/model.py
@@ -2,9 +2,9 @@ from lightai.core import *
 
 
 class Model(nn.Module):
-    def __init__(self, sz):
+    def __init__(self, sz, pretrained=True):
         super().__init__()
-        self.base = self.get_base()
+        self.base = self.get_base(pretrained)
         self.dummy_forward(sz)
         self.mean = T(np.array([20.50361 , 13.947072, 13.408824, 21.106398]).reshape((-1, 1, 1))).half()
         self.std = T(np.array([38.12811 , 39.742226, 28.598948, 38.173912]).reshape((-1, 1, 1))).half()
@@ -33,8 +33,8 @@ class Model(nn.Module):
             self.fc_bn = nn.BatchNorm1d(128)
             self.fc2 = nn.Linear(128, 28)
 
-    def get_base(self):
-        resnet = torchvision.models.resnet18(pretrained=True)
+    def get_base(self, pretrained):
+        resnet = torchvision.models.resnet18(pretrained=pretrained)
         conv1 = nn.Conv2d(4, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
         conv1.weight.data[:, :-1] = resnet.conv1.weight.data
         conv1.weight.data[:, -1] = resnet.conv1.weight.data.mean(dim=1)
