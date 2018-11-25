5b60de314375f0a033d70277f71a4455ff72fd50
Apollo <sushi90817564@gmail.com>
2018-11-25 09:02:09 +0800

    add threshold support

--- src/metric.py
+++ src/metric.py
@@ -1,26 +1,29 @@
 from lightai.core import *
 
 class F1:
-    def __init__(self, threshold):
+    def __init__(self):
         self.predicts = []
         self.targets = []
-        self.threshold = threshold
 
     def __call__(self, predict, target):
         """
         predict and target are in batch
         """
-        predict = predict > self.threshold
         self.predicts.append(predict)
         self.targets.append(target)
 
     def res(self):
-        predict = torch.cat(self.predicts).float()
+        origin_predict = torch.cat(self.predicts)
         target = torch.cat(self.targets)
-        tp = (predict*target).sum(dim=0)  #shape (28,)
-        precision = tp/(predict.sum(dim=0) + 1e-8)
-        recall = tp/(target.sum(dim=0) + 1e-8)
-        f1 = 2*(precision*recall/(precision+recall+1e-8))
+        scores = []
+        for threshold in np.linspace(0, 1, num=100, endpoint=False):
+            predict = origin_predict > threshold
+            tp = (predict*target).sum(dim=0)  #shape (28,)
+            precision = tp/(predict.sum(dim=0) + 1e-8)
+            recall = tp/(target.sum(dim=0) + 1e-8)
+            f1 = 2*(precision*recall/(precision+recall+1e-8))
+            scores.append(f1.mean())
+        scores = torch.stack(scores)
         self.predicts = []
         self.targets = []
-        return f1.mean().item()
+        return scores.max()
