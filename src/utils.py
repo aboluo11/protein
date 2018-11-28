081f5b83f9909db4cb803cbffb3620f519dfaa51
Apollo <sushi90817564@gmail.com>
2018-11-28 10:53:46 +0800

    fix bug

--- src/utils.py
+++ src/utils.py
@@ -31,3 +31,32 @@ def get_std(mean):
         items.append(item)
     std = torch.cat(items).mean(dim=0)
     return std**0.5
+
+
+def get_idx_from_target(df, target):
+    res = []
+    for i, row in enumerate(iter(df['Target'])):
+        targets = row.split()
+        for each in targets:
+            if int(each) == target:
+                res.append(i)
+                break
+    return res
+
+
+def get_sample_weight(df):
+    cls_sz = []
+    for i in range(28):
+        sz = len(get_idx_from_target(df, i))
+        cls_sz.append(sz)
+    weight = 1/np.array(cls_sz)
+    return weight
+
+
+def make_rgb(img):
+    img = img.astype(np.float32)
+    img[:,:,1] += img[:,:,0]/2
+    img[:,:,2] += img[:,:,0]/2
+    img = img[:,:,1:]
+    img = img/img.max()
+    return img
